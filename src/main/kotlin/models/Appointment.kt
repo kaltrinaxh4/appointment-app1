6a3084ff45f817041a33d80e9fb0fcc8e434342a
kaltrinaxh4 <20102209@mail.wit.ie>
2023-12-03 16:35:58 +0000

    Update appointmentDateTime to use LocalDateTime for improved date and time representation

--- src/main/kotlin/models/Appointment.kt
+++ src/main/kotlin/models/Appointment.kt
@@ -5,7 +5,7 @@ data class Appointment(
     val appointmentId: Int,
     val client: Client,
     val serviceType: String,
-    val appointmentDateTime: LocalDateTime, // Updated to use LocalDateTime for date and time
+    val appointmentDateTime: LocalDateTime, /// Updated to use LocalDateTime for date and time
     val appointmentDuration: String,
     val appointmentCostPaid: Double,
     val appointmentSatisfaction: Int,
