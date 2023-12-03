c843feab83fec2916e53ee650a5e4846242a6424
kaltrinaxh4 <20102209@mail.wit.ie>
2023-12-03 15:51:57 +0000

    Create the cancel appointment,listall appointments, listactive, listcancelled appointments

--- src/main/kotlin/models/Client.kt
+++ src/main/kotlin/models/Client.kt
@@ -50,20 +50,46 @@ private fun isValidListIndex(indexToDelete: Int, appointments: ArrayList<Appoint
 
 fun updateAppointment(indexToUpdate: Any?, note: Appointment?): Boolean {
     //find the note object by the index number
-    val foundAppointment = findAppointment(indexToUpdate)
+    val foundAppointmentID = findAppointment(indexToUpdate)
 
     //if the note exists, use the note details passed as parameters to update the found note in the ArrayList.
     val appointment = null
-    if ((foundAppointment != null) && (appointment != null)) {
+    if ((foundAppointmentID != null) && (appointment != null)) {
         val also = appointment.appointmentID.also {
             val it = ""
-            foundAppointment.appointmentID = it
+            foundAppointmentID.appointmentID = it
         }
-        foundAppointment.appointmentPriority = appointment.appointmentPriority
-        foundAppointment.appointmentClient = appointment.appointmentClient
+        foundAppointmentID.appointmentPriority = appointment.appointmentPriority
+        foundAppointmentID.appointmentClient = appointment.appointmentClient
         return true
     }
 
     //if the note was not found, return false, indicating that the update was not successful
     return false
+}
+fun cancelAppointment(indexToCancel: Any?): Boolean {
+    if (isValidIndex(indexToCancel)) {
+        val appointmentToCancel = appointments[indexToCancel]
+        if (!appointmentToCancel.isAppointmentCancelled) {
+            appointmentToCancel.isAppointmentCancelled = true
+            return true
+        }
+    }
+    return false
+}
+
+fun listAllAppointments(): String =
+    if (appointments.isEmpty())  "No appointments stored"
+    else formatListString(appointments)
+
+fun listActiveAppointments(): String =
+    if (numberOfActiveAppointments() == 0) "No active appointments stored"
+    else formatListString(appointments.filter{ appointment -> !appointment.isAppointmentCancelled })
+
+fun listCancelledAppointments(): String =
+    if (numberOfCancelledAppointments() == 0) "No cancelled appointments stored"
+    else formatListString(appointments.filter{ appointment -> appointment.isAppointmentCancelled })
+
+private fun numberOfCancelledAppointments(): Any {
+
 }
