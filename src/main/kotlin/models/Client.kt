e6133f573afc2dcc83e69063af35a418e0034a37
kaltrinaxh4 <20102209@mail.wit.ie>
2023-12-03 15:43:19 +0000

    Include the add, delete and update function in the client model

--- src/main/kotlin/models/Client.kt
+++ src/main/kotlin/models/Client.kt
@@ -29,3 +29,41 @@ data class Appointment(
     val appointmentCostPaid: Double,
     val appointmentSatisfaction: Int
 )
+//  private var serializer: Serializer = serializerType
+
+private var appointments = ArrayList<Appointment>()
+
+fun add(appointment: Appointment): Boolean {
+    return appointments.add(appointment)
+}
+
+fun deleteAppointment(indexToDelete: Any?): Appointment? {
+    return if (indexToDelete is Int && isValidListIndex(indexToDelete, appointments)) {
+        appointments.removeAt(indexToDelete)
+    } else null
+}
+
+private fun isValidListIndex(indexToDelete: Int, appointments: ArrayList<Appointment>): Boolean {
+
+    return TODO("Provide the return value")
+}
+
+fun updateAppointment(indexToUpdate: Any?, note: Appointment?): Boolean {
+    //find the note object by the index number
+    val foundAppointment = findAppointment(indexToUpdate)
+
+    //if the note exists, use the note details passed as parameters to update the found note in the ArrayList.
+    val appointment = null
+    if ((foundAppointment != null) && (appointment != null)) {
+        val also = appointment.appointmentID.also {
+            val it = ""
+            foundAppointment.appointmentID = it
+        }
+        foundAppointment.appointmentPriority = appointment.appointmentPriority
+        foundAppointment.appointmentClient = appointment.appointmentClient
+        return true
+    }
+
+    //if the note was not found, return false, indicating that the update was not successful
+    return false
+}
