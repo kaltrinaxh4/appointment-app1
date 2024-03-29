7aa04e9b6c9edfb658b3a683efd4079cfd471d32
kaltrinaxh4 <20102209@mail.wit.ie>
2024-03-29 19:56:16 +0000

    Implement the updateClient function in ClientAPI to update client information

--- src/main/kotlin/controllers/ClientAPI.kt
+++ src/main/kotlin/controllers/ClientAPI.kt
@@ -68,3 +68,16 @@ class ClientAPI(serializerType: Serializer) {
             clients.filter { client -> client.phone == searchInt }
         )
 }
+fun updateClient(id: Int, client: Client?): Boolean {
+    val foundClient = findClientById(id)
+    if ((foundClient != null) && (client != null)) {
+        foundClient.firstName = client.firstName
+        foundClient.lastName = client.lastName
+        foundClient.address = client.address
+        foundClient.email = client.email
+        foundClient.phone = client.phone
+        foundClient.extraInfo = client.extraInfo
+        return true
+    }
+    return false
+}
