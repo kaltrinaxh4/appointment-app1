f211f88e85a65f4e8a4b15ef3084c9f896269448
kaltrinaxh4 <20102209@mail.wit.ie>
2024-03-29 14:55:35 +0000

    Implement Appointment toString Method

--- src/main/kotlin/models/Appointment.kt
+++ src/main/kotlin/models/Appointment.kt
@@ -3,7 +3,16 @@ data class Appointment(var appointmentId: Int = 0,
                        var date: String,
                        var time: Double,
                        var treatment: String,
-                       var cost: Int,
-                       var rating: Int,
+                       var price: Int,
+                       var review: Int,
                        var isScheduled: Boolean)
 {
+    override fun toString() =
+        if (isScheduled)
+            "$appointmentId: $date $time  $treatment $price $review(Scheduled)"
+        else
+            "$appointmentId: $time $treatment $price $review (Not Available)"
+
+
+}
+}
