4032b26519807d09676f01036243552b429ddf29
kaltrinaxh4 <20102209@mail.wit.ie>
2024-03-30 01:02:54 +0000

    I changed my gradle by adding the plugin for Dokka. I added implementations for streaming to XML and JSON and generating a dokka site

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,18 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.9.0"
+    // Plugin for Dokka - KDoc generating tool
+    id("org.jetbrains.dokka") version "1.9.10"
+    // Code coverage tool
+    jacoco
+    // Plugin for Ktlint
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
     application
 }
 
-group = "me.kaltr"
-version = "1.0-SNAPSHOT"
+group = "ie.setu"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -12,16 +20,41 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("io.github.microutils:kotlin-logging:3.0.5")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+    // For Streaming to XML and JSON
+    implementation("com.thoughtworks.xstream:xstream:1.4.18")
+    implementation("org.codehaus.jettison:jettison:1.4.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    // For generating a Dokka Site from KDoc
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
 }
 
 tasks.test {
     useJUnitPlatform()
+    // report is always generated after tests run
+    finalizedBy(tasks.jacocoTestReport)
 }
 
 kotlin {
     jvmToolchain(8)
 }
 
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
 application {
     mainClass.set("MainKt")
+}
+
+tasks.jar {
+    manifest.attributes["Main-Class"] = "MainKt"
+    // for building a fat jar - include all dependencies
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(sourceSets.main.get().output)
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
 }
