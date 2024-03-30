ff0632ee3e20f4809176616e88a00f6f138a38f5
kaltrinaxh4 <20102209@mail.wit.ie>
2024-03-30 01:12:13 +0000

    Add the client API variable to hold it as an object

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,7 @@
 import controllers.ClientAPI
 import models.Appointment
 import models.Client
+import persistence.XMLSerializer
+import java.io.File
+
+private val clientAPI = ClientAPI(XMLSerializer(File("notes.xml")))
