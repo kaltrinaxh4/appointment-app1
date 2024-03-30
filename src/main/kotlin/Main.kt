7b6912bfe7d89f6cc256fb5921fd870e7408f0ec
kaltrinaxh4 <20102209@mail.wit.ie>
2024-03-30 02:11:35 +0000

    In runMenu include a val category by choice to allow me to choose betwen three menus

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,3 +7,16 @@ import java.io.File
 private val clientAPI = ClientAPI(XMLSerializer(File("notes.xml")))
 
 fun main() = runMenu()
+fun runMenu() {
+    do {
+        val categoryChoice = readCategoryChoice()
+
+        when (categoryChoice) {
+            1 -> runClientMenu()
+            2 -> runAppointmentMenu()
+            3 -> runSearchingMenu()
+            0 -> exitApp()
+            else -> println("Invalid menu choice: $categoryChoice")
+        }
+    } while (true)
+}
