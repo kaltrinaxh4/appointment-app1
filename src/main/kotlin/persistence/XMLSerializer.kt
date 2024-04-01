9f925e4137cfb40a4550e529bf2ec901f4ca63e9
kaltrinaxh4 <20102209@mail.wit.ie>
2024-04-01 22:41:23 +0100

    I included persistence code that I had from a past project called notes app. Here i did the XMLserializer

--- src/main/kotlin/persistence/XMLSerializer.kt
+++ src/main/kotlin/persistence/XMLSerializer.kt
@@ -1,25 +1,30 @@
 package persistence
 
-import java.io.File
-import kotlin.Throws
+/**
+ * 1. Imports XStream and XML library
+ */
 import com.thoughtworks.xstream.XStream
 import com.thoughtworks.xstream.io.xml.DomDriver
-import models.Note
+import models.Appointment
+import models.Client
+import java.io.File
 import java.io.FileReader
 import java.io.FileWriter
-import java.lang.Exception
 
-class XMLSerializer(private val file: File) : Serializer {
+class XMLSerializer(private val file: File) : Serializer
+{
 
     @Throws(Exception::class)
     override fun read(): Any {
         val xStream = XStream(DomDriver())
-        xStream.allowTypes(arrayOf(Note::class.java))
+        xStream.allowTypes(arrayOf(Client::class.java))
+        xStream.allowTypes(arrayOf(Appointment::class.java))
         val inputStream = xStream.createObjectInputStream(FileReader(file))
         val obj = inputStream.readObject() as Any
         inputStream.close()
         return obj
     }
+
 
     @Throws(Exception::class)
     override fun write(obj: Any?) {
@@ -28,4 +33,6 @@ class XMLSerializer(private val file: File) : Serializer {
         outputStream.writeObject(obj)
         outputStream.close()
     }
+
+
 }
