bf86bb5f79259f5812ae27b3adf80d0b879505ec
kaltrinaxh4 <20102209@mail.wit.ie>
2024-04-01 22:41:10 +0100

    I included persistence code that I had from a past project called notes app. Here i did the serializer

--- src/main/kotlin/persistence/Serializer.kt
+++ src/main/kotlin/persistence/Serializer.kt
@@ -1,10 +1,12 @@
 package persistence
 
-interface Serializer {
+interface Serializer
+{
+
     @Throws(Exception::class)
     fun write(obj: Any?)
+
 
     @Throws(Exception::class)
     fun read(): Any?
 }
-//
