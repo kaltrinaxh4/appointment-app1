e8a4f164c363e9fa0ea7d78fd1e740a160456df7
kaltrinaxh4 <20102209@mail.wit.ie>
2024-04-01 18:00:59 +0100

    Refactor category utility tests and update category utility based on requirements

--- src/test/kotlin/utils/CategoryUtilityTest.kt
+++ src/test/kotlin/utils/CategoryUtilityTest.kt
@@ -6,25 +6,35 @@ import utils.CategoryUtility.categories
 import utils.CategoryUtility.isValidCategory
 
 internal class CategoryUtilityTest {
+
     @Test
-    fun categoriesReturnsFullCategoriesSet(){
-        Assertions.assertEquals(2, categories.size)
-        Assertions.assertTrue(categories.contains("Home"))
-        Assertions.assertTrue(categories.contains("College"))
-        Assertions.assertFalse(categories.contains(""))
+    fun categoriesReturnsFullCategoriesSet() {
+        Assertions.assertEquals(5, categories.size)
+        Assertions.assertTrue(categories.contains("Hairdye"))
+        Assertions.assertTrue(categories.contains("Haircut"))
+        Assertions.assertTrue(categories.contains("Blowdry"))
+        Assertions.assertTrue(categories.contains("KeratinTreatment"))
+        Assertions.assertTrue(categories.contains("HairPerm"))
+        Assertions.assertFalse(categories.contains("Threading"))
     }
 
     @Test
-    fun isValidCategoryTrueWhenCategoryExists(){
-        Assertions.assertTrue(isValidCategory("Home"))
-        Assertions.assertTrue(isValidCategory("home"))
-        Assertions.assertTrue(isValidCategory("COLLEGE"))
+    fun isValidCategoryTrueWhenCategoryExists() {
+        Assertions.assertTrue(isValidCategory("hairdye"))
+        Assertions.assertTrue(isValidCategory("haircut"))
+        Assertions.assertTrue(isValidCategory("blowdry"))
+        Assertions.assertTrue(isValidCategory("keratintreatment"))
+        Assertions.assertTrue(isValidCategory("hairperm"))
+        Assertions.assertFalse(isValidCategory("threading"))
     }
 
     @Test
-    fun isValidCategoryFalseWhenCategoryDoesNotExist(){
-        Assertions.assertFalse(isValidCategory("Hom"))
-        Assertions.assertFalse(isValidCategory("colllege"))
-        Assertions.assertFalse(isValidCategory(""))
+    fun isValidCategoryFalseWhenCategoryDoesNotExist() {
+        Assertions.assertFalse(isValidCategory("Haird"))
+        Assertions.assertFalse(isValidCategory("Hairc"))
+        Assertions.assertFalse(isValidCategory("Blowd"))
+        Assertions.assertFalse(isValidCategory("Keratin"))
+        Assertions.assertFalse(isValidCategory("Hairp"))
+        Assertions.assertFalse(isValidCategory("Hairperming"))
     }
 }
