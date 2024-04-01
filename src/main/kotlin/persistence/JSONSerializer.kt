562ffaa8d4306d8d1fe4796b947c3d771227bb23
kaltrinaxh4 <20102209@mail.wit.ie>
2024-04-01 22:40:58 +0100

    I included persistence code that I had from a past project called notes app. Here i did the JSONserializer

--- src/main/kotlin/persistence/JSONSerializer.kt
+++ src/main/kotlin/persistence/JSONSerializer.kt
@@ -2,21 +2,26 @@ package persistence
 
 import com.thoughtworks.xstream.XStream
 import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver
-import models.Note
+import models.Appointment
+import models.Client
 import java.io.File
 import java.io.FileReader
 import java.io.FileWriter
 
-class JSONSerializer(private val file: File) : Serializer {
+class JSONSerializer(private val file: File) : Serializer
+{
+
     @Throws(Exception::class)
     override fun read(): Any {
         val xStream = XStream(JettisonMappedXmlDriver())
-        xStream.allowTypes(arrayOf(Note::class.java))
+        xStream.allowTypes(arrayOf(Client::class.java))
+        xStream.allowTypes(arrayOf(Appointment::class.java))
         val inputStream = xStream.createObjectInputStream(FileReader(file))
         val obj = inputStream.readObject() as Any
         inputStream.close()
         return obj
     }
+
 
     @Throws(Exception::class)
     override fun write(obj: Any?) {
